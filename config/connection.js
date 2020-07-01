73631faeaa6bbe3976b62ec0f15a458934907034
Chris Salgado <musicman785@gmail.com>
2020-07-01 08:18:34 -0700

    Corrected connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 // Set up MySQL connection.
 
 const mysql = require("mysql");
-const connection;
 
-// Connects to mysql database 
+// Connects to mysql database through jawsDB
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host:"localhost",
+    port: 3306,
     user: "root",
     password:"Rudim3nts08",
     database:"burgers_db"
@@ -16,13 +16,7 @@ if (process.env.JAWSDB_URL) {
 };
 
 // Make connection.
-connection.connect(err => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
